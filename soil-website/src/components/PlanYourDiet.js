2c11d77d33fa09f8740b8a28dd53ede9a784f82b
21447sadf <s3946776@student.rmit.edu.au>
2024-03-24 00:48:27 +1100

    Built PlanYourDiet component for Homepage

--- soil-website/src/components/PlanYourDiet.js
+++ soil-website/src/components/PlanYourDiet.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 function PlanYourDiet() {
+
+    //STYLING
+    //Text
+    const textStyle = {
+        position: "absolute",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        top: "0px",
+        left: "0px",
+        height: "1000px",
+        width: "700px",
+        backgroundColor: "#FFF9C7"
+    };
+
+    //Image 
+    const imageStyle = {
+        position: "relative",
+        width: "100%",
+        height: "1000px",
+        fit: "cover",
+    };
+
     return (
-        <p>Plan Your Diet Info Goes Here</p>
+        //Image wrapper
+        <div className="PlanYourDietWrapper" style={imageStyle}>
+            <img src="MealPlanning.jpg" alt="MealPlanning.jpg" height="1000px" width="100%"></img>
+            {/* Text Wrapper */}
+            <div className="text" style={textStyle}>
+                <h1>Plan your nutritious diet!</h1>
+            </div>
+        </div>
     );
 }
 
--- soil-website/src/pages/home.js
+++ soil-website/src/pages/home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Slideshow from "../components/Slideshow";
-import Sales from "../components/Sales";
+import PlanYourDiet from "../components/PlanYourDiet";
 
 function Home() {
     return (
         <div className="Home">
             <p>Home page</p>
             <Slideshow />
+            <PlanYourDiet />
         </div>
     );
 }
