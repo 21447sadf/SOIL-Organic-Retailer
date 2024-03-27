d43b3eaff3e9f9af0884be6435a2a860546d9d9e
21447sadf <s3946776@student.rmit.edu.au>
2024-03-28 10:57:50 +1100

    Added routing to SignInPage

--- soil-website/src/App.js
+++ soil-website/src/App.js
@@ -1,32 +1,37 @@
-import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Sales from "./pages/Sales";
+import SignInPage from "./pages/SignInPage";
 import DIYToolkit from "./pages/DIYToolkit";
 import PlanYourDiet from "./components/PlanYourDiet";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Navbar />
+      
       {/* /* Set up router paths */}
-      <Router>
+      <BrowserRouter>
+        <Navbar />
         <Routes>
+          
           <Route exact path="/" element={<Home />} />
           <Route path="/Sales" element={<Sales />} />
           <Route path="/PlanYourDiet" element={<PlanYourDiet/>} />
           <Route path="/DIYToolkit" element={<DIYToolkit />} />
+          <Route path="/SignInPage" element={<SignInPage />} />
         </Routes>
-      </Router>
 
-      <SignIn />
-      <SignUp />
+      </BrowserRouter>
+
+
+
       <Footer /> 
     </div>
   );
---
+++ soil-website/src/pages/SignInPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//This is the sign-in page for users
+
+function signInPage() {
+    return (
+        <p>signInPage goes here</p>
+            
+    );
+}
+
+export default signInPage;
