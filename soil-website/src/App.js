7c0f3e382118f4b45e67ccbb42434478c405a1d3
LabibaShaikh <s3954789@student.rmit.edu.au>
2024-03-28 14:30:09 +1100

    finished merging for now

--- soil-website/src/App.js
+++ soil-website/src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path ="/SignIn" element={<SignIn />} />
         </Routes>
 
-      </Router>
+      </BrowserRouter>
 
       <Footer /> 
     </div>
--- soil-website/src/components/SignIn.js
+++ soil-website/src/components/SignIn.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 function SignIn() {
     return (
         <div className="signInBtn">
-            <Link to="/SignInPage">
+           
                 <button>Sign In</button>
-            </Link>  
+           
         </div>
     ); 
 }
