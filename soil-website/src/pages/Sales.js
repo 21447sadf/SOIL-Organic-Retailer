c8c1c447a2f09a5a9f86ad17d11bd9daf3775f6c
21447sadf <s3946776@student.rmit.edu.au>
2024-03-31 01:30:46 +1100

    Added new Sale item (Wholemead bread) to Specials Page

--- soil-website/src/pages/Sales.js
+++ soil-website/src/pages/Sales.js
@@ -16,35 +16,42 @@ function Sales() {
                     <a href="/">
                         <img src="./Corn.jpg" alt="Corn" width="450" height="300" />
                     </a>
-                    <div class="desc">6 Pack of Corn <br/><br/><b>$5.00</b></div>
+                    <div class="desc">6 Pack of Corn <br/><br/><b>$3.00</b></div>
                     <button class="Add-to-cart-btn">Add to cart</button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./MuesliBar.jpg" alt="MuesliBar" width="450" height="300" />
                     </a>
-                    <div class="desc">HealthNuts Muesli Bar - 5 pack <br/><br/><b>$7.00</b></div>
+                    <div class="desc">HealthNuts Muesli Bar - 5 pack <br/><br/><b>$3.50</b></div>
                     <button class="Add-to-cart-btn">Add to cart</button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./Pasta.jpg" alt="Pasta" width="450" height="300" />
                     </a>
-                    <div class="desc">Nature's Noodle Co. - 500g High Protein Pasta <br/><b>$8.00</b></div>
+                    <div class="desc">Nature's Noodle Co. - 500g High Protein Pasta <br/><b>$4.00</b></div>
                     <button class="Add-to-cart-btn">Add to cart</button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./Spinach.jpg" alt="Spinach" width="450" height="300" />
                     </a>
-                    <div class="desc">Fresh Spinach - 300g <br/><br/><b>$6.50</b></div>
+                    <div class="desc">Fresh Spinach - 300g <br/><br/><b>$2.50</b></div>
                     <button class="Add-to-cart-btn">Add to cart</button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./Yoghurt.jpg" alt="Yoghurt" width="450" height="300" />
                     </a>
-                    <div class="desc">Yokos Unsweetened Yoghurt - 500g <br/><br/><b>$8.00</b></div>
+                    <div class="desc">Yokos Unsweetened Yoghurt - 500g <br/><br/><b>$2.50</b></div>
+                    <button class="Add-to-cart-btn">Add to cart</button>
+                </div>
+                <div class="Sale-Item">
+                    <a href="/">
+                        <img src="./bread.jpg" alt="bread" width="450" height="300" />
+                    </a>
+                    <div class="desc">Dough Delight Co. Wholemeal Bread Loaf <br/><b>$3.50</b></div>
                     <button class="Add-to-cart-btn">Add to cart</button>
                 </div>
             </div>
