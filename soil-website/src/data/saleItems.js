4241074b05e4d0365d50381f2d2972118866cc3c
21447sadf <s3946776@student.rmit.edu.au>
2024-04-01 23:12:58 +1100

    Created saleItems - for localStorage of items

---
+++ soil-website/src/data/saleItems.js
@@ -0,0 +1,8 @@
+const saleItems = [
+    { id: 1, prodName: "Large Corn - 6 pack", price: "3.00" },
+    { id: 2, prodName: "HealthNuts Muesli Bar - 5 pack", price: "3.50" },
+    { id: 3, prodName: "Nature's Noodle Co. - 500g High Protein Pasta", price: "4.00" },
+    { id: 4, prodName: "Fresh Spinach - 300g", price: "2.50" },
+    { id: 5, prodName: "Yokos Unsweetened Yoghurt - 500g", price: "3.00" },
+    { id: 6, prodName: "Dough Delight Co. Wholemeal Bread Loaf", price: "3.50" }
+];
