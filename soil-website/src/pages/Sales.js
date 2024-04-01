96bb5c151ae070c5fcbc5892d2bdec9d1b1c0b7f
21447sadf <s3946776@student.rmit.edu.au>
2024-04-02 03:07:20 +1100

    Implemented 'Add to cart' button

--- soil-website/src/pages/Sales.js
+++ soil-website/src/pages/Sales.js
@@ -1,13 +1,37 @@
-import React from "react";
+import { useState } from "react";
 import "./Sales.css"
-import initSaleItems from "../data/saleItems";
 
 //This component includes a list of sale items
 
 //TO DO: Redirect to Shopping Cart, with item added in
+const ITEMS_KEY = "items";
 
 function Sales() {
-    initSaleItems();
+    //Hook to add items to cart
+    const [cartItems, setCartItems] = useState([]);
+
+    //Function to add item to cart
+    function addItemToCart(item) {
+        //Check if item already exists in local storage
+        //If it does, don't add anything
+        const items = JSON.parse(localStorage.getItem(ITEMS_KEY));
+        if (items !== null) {
+            if (items.find(obj => obj.prodName === item.prodName) !== undefined) {
+                return cartItems;
+            }
+        }
+
+        //Use callback to update local storage
+        setCartItems(cartItems => {
+            cartItems = [...cartItems, item];
+            //Store item in local storage
+            localStorage.setItem(ITEMS_KEY, JSON.stringify(cartItems));
+            return cartItems;
+        });
+      
+        
+    };
+
     return (
         <div class="Sales-Container">
             <div class="Sales">
@@ -18,43 +42,61 @@ function Sales() {
                     <a href="/">
                         <img src="./Corn.jpg" alt="Corn" width="450" height="300" />
                     </a>
-                    <div class="desc">6 Pack of Corn <br/><br/><b>$3.00</b></div>
-                    <button class="Add-to-cart-btn">Add to cart</button>
+                    <div class="desc">Large Corn - 6 pack <br/><br/><b>$3.00</b></div>
+                    <button class="Add-to-cart-btn" 
+                            onClick={() => addItemToCart({id: "1", prodName: "Large Corn - 6 pack", price: 3.00, qty: 1})}>
+                                Add to cart
+                    </button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./MuesliBar.jpg" alt="MuesliBar" width="450" height="300" />
                     </a>
                     <div class="desc">HealthNuts Muesli Bar - 5 pack <br/><br/><b>$3.50</b></div>
-                    <button class="Add-to-cart-btn">Add to cart</button>
+                    <button class="Add-to-cart-btn"
+                            onClick={() => addItemToCart({id: "2", prodName: "HealthNuts Muesli Bar - 5 pack", price: 3.50, qty: 1})}>
+                                Add to cart
+                    </button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./Pasta.jpg" alt="Pasta" width="450" height="300" />
                     </a>
                     <div class="desc">Nature's Noodle Co. - 500g High Protein Pasta <br/><b>$4.00</b></div>
-                    <button class="Add-to-cart-btn">Add to cart</button>
+                    <button class="Add-to-cart-btn"
+                            onClick={() => addItemToCart({id: "3", prodName: "Nature's Noodle Co. - 500g High Protein Pasta", price: 4.00, qty: 1})}>
+                                Add to cart
+                    </button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./Spinach.jpg" alt="Spinach" width="450" height="300" />
                     </a>
                     <div class="desc">Fresh Spinach - 300g <br/><br/><b>$2.50</b></div>
-                    <button class="Add-to-cart-btn">Add to cart</button>
+                    <button class="Add-to-cart-btn"
+                            onClick={() => addItemToCart({id: "4", prodName: "Fresh Spinach - 300g", price: 4.00, qty: 1})}>
+                                Add to cart
+                    </button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./Yoghurt.jpg" alt="Yoghurt" width="450" height="300" />
                     </a>
                     <div class="desc">Yokos Unsweetened Yoghurt - 500g <br/><br/><b>$2.50</b></div>
-                    <button class="Add-to-cart-btn">Add to cart</button>
+                    <button class="Add-to-cart-btn"
+                            onClick={() => addItemToCart({id: "5", prodName: "Yokos Unsweetened Yoghurt - 500g", price: 2.50, qty: 1})}>
+                                Add to cart
+                    </button>
                 </div>
                 <div class="Sale-Item">
                     <a href="/">
                         <img src="./bread.jpg" alt="bread" width="450" height="300" />
                     </a>
                     <div class="desc">Dough Delight Co. Wholemeal Bread Loaf <br/><b>$3.50</b></div>
-                    <button class="Add-to-cart-btn">Add to cart</button>
+                    <button class="Add-to-cart-btn"
+                            onClick={() => addItemToCart({id: "6", prodName: "Dough Delight Co. Wholemeal Bread Loaf", price: 3.50, qty: 1})}>
+                                Add to cart
+                    </button>
                 </div>
             </div>
         </div>
--- soil-website/src/pages/ShoppingCart.js
+++ soil-website/src/pages/ShoppingCart.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 function ShoppingCart() {
+    //Access cartItems from localStorage and display
+    const cartItems = JSON.parse(localStorage.getItem('items'));
+    let total = 0;
+
+    //State to update quantity field
+    // [qty, setQty] = useState(0);
+
+    // const increaseQty = (qty) => {
+    //     setQty(qty + 1);
+    // };
+
+
     return (
         <div className="cartWrapper">
             <h1>Your cart</h1>
 
-            {/* <div className="shoppingCartTable">
+            <div className="shoppingCartTable">
                 <table>
                     <tr>
                         <th>Product</th>
@@ -13,19 +26,36 @@ function ShoppingCart() {
                         <th>Quantity</th>
                         <th>Total</th>
                     </tr>
+                    {/*Display all cart items as rows here*/}
+                    {cartItems !== null ? (cartItems.map((item, index) => (
+                        <tr>
+                            <td>{item.prodName}</td>
+                            <td>{item.price}</td>
+                            <td>
+                                <button>-</button>
+                                <input type="number" id="number" value={item.qty} />
+                                <button onclick={increaseQty(item.qty)}>+</button>
+                            </td>
+                            <td>{item.price * item.qty}</td>
+                        </tr>
+                    ))
+                ) :  (
                     <tr>
+                        <td colSpan="4">No items in the cart</td>
+                    </tr>)}
+                    {/* <tr>
                         <td>Product Image + Name Goes here</td>
                         <td>Price goes here</td>
                         <td>Quantity + Option to change qty.</td>
                         <td>Total price goes here</td>
-                    </tr>
+                    </tr> */}
                 </table>
             </div>
             <div className="Order-Summary">
                 <table>
                     <th>Order Summary</th>
-                    <tr>Subtotal<td>$100.00</td></tr>
-                    <tr>TOTAL<td>$110.00</td></tr>
+                    <tr>Subtotal<td>${total}</td></tr>
+                    <tr>TOTAL<td>${total}</td></tr>
                 </table>
             </div>
             <div className="checkout-Btn">
@@ -33,7 +63,7 @@ function ShoppingCart() {
             </div>
             <div className="continue-Shopping-Btn">
                 <button>Continue Shopping</button>
-            </div> */}
+            </div>
         </div>
     );
 }
