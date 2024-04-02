473da85ea45c1dfa8214fa1542ee2b98f2dfc5f9
21447sadf <s3946776@student.rmit.edu.au>
2024-04-02 14:12:43 +1100

    Added removeItemFromCart function to ShoppingCart

--- soil-website/src/pages/ShoppingCart.js
+++ soil-website/src/pages/ShoppingCart.js
@@ -1,18 +1,30 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChangeQtyBtn from "../components/changeQtyBtn";
+
+const ITEMS_KEY = "items";
 
 function ShoppingCart() {
     //Access cartItems from localStorage and display
     const cartItems = JSON.parse(localStorage.getItem('items'));
     let total = 0;
+    
+    //Hook to add items to cart
+        //Hook to add items to cart
+        const [items, setCartItems] = useState(JSON.parse(localStorage.getItem(ITEMS_KEY)));
 
-    //Function to update price on each product
-    // const [price, setPrice] = useState(0);
-
-    // function updatePrice(qty, price) {
-    //     setPrice((prevPrice) => (qty * price));
-    // };
+        //Function to add item to cart
+        function removeItemFromCart(item) {
+            //Use callback to remove item and update local storage
+            setCartItems(items => {
+                items = items.filter(obj => obj.prodName !== item.prodName);
+                //Store item in local storage
+                localStorage.setItem(ITEMS_KEY, JSON.stringify(items));
+                return items;
+            });
+          
+            
+        };
 
     return (
         <div className="cartWrapper">
@@ -27,39 +39,21 @@ function ShoppingCart() {
                         <th>Total</th>
                     </tr>
                     {/*Display all cart items as rows here*/}
-                    {cartItems !== null ? (cartItems.map((item, index) => (
+                    {( cartItems && cartItems.length > 0) ? (cartItems.map((item, index) => (
                         <tr>
                             <td>{item.prodName}</td>
                             <td>{item.price}</td>
                             <td>
                                 <ChangeQtyBtn minValue={1} maxValue={10} />
-                            {/* <div className="btn-group">
-                                <button className="increment-btn" onClick={handleDecrementCounter}>
-                                    <span class="material-symbols-outlined">-</span>
-                                </button>
-                                <p>{item.qty}</p>
-                                <button className="decrement-btn" onClick={handleIncrementCounter}>
-                                    <span class="material-symbols-outlined">+</span>
-                                </button>
-                                </div> */}
-                                {/* <button>-</button>
-                                <input type="number" id="number" value={item.qty} />
-                                <button onclick= {increaseQty(item.qty)}>+</button> */}
                             </td>
                             <td>{item.price * item.qty}</td>
-                            <td><button>X</button></td>
+                            <td><button onClick={() => removeItemFromCart(item)}>X</button></td>
                         </tr>
                     ))
                 ) :  (
                     <tr>
-                        <td colSpan="4">No items in the cart</td>
+                        <td>No items in the cart</td>
                     </tr>)}
-                    {/* <tr>
-                        <td>Product Image + Name Goes here</td>
-                        <td>Price goes here</td>
-                        <td>Quantity + Option to change qty.</td>
-                        <td>Total price goes here</td>
-                    </tr> */}
                 </table>
             </div>
             <div className="Order-Summary">
