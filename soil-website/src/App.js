80cc472ef36cc293e6a2adee6a230ee32899416f
LabibaShaikh <s3954789@student.rmit.edu.au>
2024-04-05 11:26:12 +1100

    Fixed Profile button actions

--- soil-website/src/App.js
+++ soil-website/src/App.js
@@ -1,37 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import SignIn from "./components/SignIn";
 import Sales from "./pages/Sales";
-import ShoppingCart from './pages/ShoppingCart';
-import SignInPage from "./pages/SignInPage";
+import ShoppingCart from "./pages/ShoppingCart";
 import DIYToolkit from "./pages/DIYToolkit";
 import PlanYourDiet from "./components/PlanYourDiet";
 import Footer from "./components/Footer";
 import Home from "./pages/home";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+
       {/* /* Set up router paths */}
       <BrowserRouter>
         <Navbar />
         <Routes>
-          
           <Route exact path="/" element={<Home />} />
           <Route path="/Sales" element={<Sales />} />
-          <Route path="/PlanYourDiet" element={<PlanYourDiet/>} />
+          <Route path="/PlanYourDiet" element={<PlanYourDiet />} />
           <Route path="/DIYToolkit" element={<DIYToolkit />} />
-          <Route path="/SignInPage" element={<SignInPage />} />
-          <Route path ="/SignIn" element={<SignIn />} />
-          <Route path ="/ShoppingCart" element={<ShoppingCart />} />
+
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/ShoppingCart" element={<ShoppingCart />} />
         </Routes>
       </BrowserRouter>
 
-      <Footer /> 
+      <Footer />
     </div>
   );
 }
