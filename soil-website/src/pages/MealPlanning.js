13ceb322a886f40dd4e329f93372cc108d6562a0
21447sadf <s3946776@student.rmit.edu.au>
2024-04-18 16:15:33 +1000

    Testing Spoonacular API

--- soil-website/src/pages/MealPlanning.js
+++ soil-website/src/pages/MealPlanning.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function MealPlanning() {
+    //API to fetch recipes from
+    const URL = "https://api.spoonacular.com/recipes/complexSearch";
+
+    //Hooks to manage the retrieval of recipes from the API
+    const [meals, setMeals] = useState(null);
+    const [getMeals, setGetMeals] = useState(true);
+
+    //Use Effect to call the API
+    useEffect(() => {
+        if(getMeals) {
+        //Use promise to call the API asynchronously
+        fetch(URL).then(response => response.json()).then(results => {
+            setMeals(results);
+            setGetMeals(false);
+        });
+        }
+    }, [getMeals]);
+
+    //Button function to handle submitting requests 
+    const submitRequest = () => {
+        setMeals(null);
+        setGetMeals(true);
+    }
+
     return (
-        <p>Meal Planning info goes here</p>
+        <div className="PersonaliseMealsForm">
+            {/* Conditionally render loading message, or results from API */}
+            {
+                meals === null ? 
+                <p>Please wait while we fetch some meals for you...</p>
+                :
+                <div>
+                    {meals.size}
+                    {/* {meals.map(meals => (
+                        <li key = {meals.id}>
+                            {meals.minProteinPercent} | {meals.maxProteinPercent}
+                        </li>
+                    ))} */}
+                </div>
+                }
+            <label for="MealInput">Enter your meal type:</label>
+            <input id="MealInput "type="Meal" placeholder="Enter meal here" />
+            <button className="Submit-Meals-Request" onClick={submitRequest}>Find meals</button>
+        </div>
     );
 };
 
