0f28e4fe483bccfa9d77b17f225e62a93e3bd485
21447sadf <s3946776@student.rmit.edu.au>
2024-04-21 15:15:47 +1000

    Revert "fixed dynamic display lesgoooooo"
    This reverts commit 5703a45786fb77af151b9f71f1aba300e31963e0.

--- soil-website/src/components/Navbar.js
+++ soil-website/src/components/Navbar.js
@@ -7,90 +7,52 @@ import "./Profile";
 import "./SignIn";
 import "./Navbar.css";
 import {Link} from "react-router-dom";
-import profilr_icon from './profile.png';
+import profile_icon from './profile.png';
 function Navbar(i) {
     // const[isUserLoggedIn, setIsUserLoggedIn] = useState(false);
     // // const isAuthenticated = localStorage.getItem('isLoggedIn') === 'true';
-   
-    // useEffect(() => {
-    //   const profile = localStorage.getItem('profile');
-    //   setIsUserLoggedIn(profile!== null);
-    // }, []);
 
-    // useEffect(() => {
-      
-    //   const checkAuthStatus = () => {
-    //     const loggedIn = localStorage.getItem('isLoggedIn') === 'true';
-    //     setIsUserLoggedIn(loggedIn);
-    //   };
-
-    
-     
-    //   window.addEventListener('storage', checkAuthStatus);
-    
-    
-    //   checkAuthStatus();
-    
-    //   const { isUserLoggedIn } = useAuth(); // Use the hook to get auth state
-    //   return () => window.removeEventListener('storage', checkAuthStatus);
-    // }, []);
-
-    const { isUserLoggedIn } = useAuth(); // Use the hook to get auth state directly
-
-  return (
-    <div className = "navbarWrapper">
-    <nav className = "navbar">
-      <div className = "listWrapper">
-        <ul className="no_style_list">
-          <li>
-            <a href="/">Home</a>
-            <a href="/Sales">Specials</a>
-            <a href="/MealPlanning ">Plan your diet</a>
-            <a href="/DIYToolkit ">DIY Gardening</a>
-          </li>
-        </ul>
-      </div>
-
-      {isUserLoggedIn? (
-        <div>
-          <Link to="/Profile">
-           <img src = {profilr_icon} alt = "Profile" id = "profile-icon" />
-          </Link>
-          <Link to="/ShoppingCart">
-        <img 
-        src="../shopping_cart_black_24dp.svg" 
-        alt="shopping-cart-btn"
-        id="shopping-cart-btn">
-        </img>
-      </Link>
-
+      const { isUserLoggedIn } = useAuth(); // Use the hook to get auth state directly
+  
+      return (
+        <div className="navbarWrapper">
+          <nav className="navbar">
+            <div className="listWrapper">
+              <ul className="no_style_list">
+                <li><a href="/">Home</a></li>
+                <li><a href="/Sales">Specials</a></li>
+                <li><a href="/MealPlanning">Plan your diet</a></li>
+                <li><a href="/DIYToolkit">DIY Gardening</a></li>
+              </ul>
+            </div>
+  
+            <div>
+              {isUserLoggedIn ? (
+                <>
+                  <Link to="/Profile">
+                    <img src={profile_icon} alt="Profile" id="profile-icon" />
+                  </Link>
+                  <Link to="/ShoppingCart">
+                    <img src="../shopping_cart_black_24dp.svg" alt="shopping-cart-btn" id="shopping-cart-btn" />
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/SignIn">
+                    <button id="SignIn-Btn">Sign In</button>
+                  </Link>
+                  <Link to="/SignUp">
+                    <button id="SignUp-Btn">Sign Up</button>
+                  </Link>
+                  <Link to="/ShoppingCart">
+                    <img src="../shopping_cart_black_24dp.svg" alt="shopping-cart-btn" id="shopping-cart-btn" />
+                  </Link>
+                </>
+              )}
+            </div>
+          </nav>
         </div>
-        ) : (
-        <div>
-          
-      <Link to="/SignIn">
-        <button id="SignIn-Btn">Sign In</button>
-       
-      </Link>
-      <Link to="/SignUp">
-        <button id="SignUp-Btn">Sign Up</button>
-      </Link>
-      <Link to="/ShoppingCart">
-        <img 
-        src="../shopping_cart_black_24dp.svg" 
-        alt="shopping-cart-btn"
-        id="shopping-cart-btn">
-        </img>
-      </Link>
-      </div>
-        )}
-    </nav>
-   </div>
-
- 
-
-    
-  );
-}
+      );
+  }
 
 export default Navbar;
